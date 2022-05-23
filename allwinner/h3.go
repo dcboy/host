d935222524b360e5a70a52afca436f21e01b8d35
DC <szdcboy@gmail.com>
2022-05-23 18:40:01 +0800

    fix: update h3.go

--- allwinner/h3.go
+++ allwinner/h3.go
@@ -20,11 +20,17 @@ var mappingH3 = map[string][5]pin.Func{
 	"PA8":  {"SIM_DATA", "", "", "", "PA_EINT8"},
 	"PA9":  {"SIM_RST", "", "", "", "PA_EINT9"},
 	"PA10": {"SIM_DET", "", "", "", "PA_EINT10"},
+	"PA12": {"TWI0_SDA", "DI_RX", "", "", "PA_EINT12"},
 	"PA17": {"OWA_OUT", "", "", "", "PA_EINT17"},
 	"PA18": {"PCM0_SYNC", "TWI1_SCK", "", "", "PA_EINT18"},
 	"PA19": {"PCM0_CLK", "TWI1_SDA", "", "", "PA_EINT19"},
 	"PA20": {"PCM0_DOUT", "SIM_VPPEN", "", "", "PA_EINT20"},
 	"PA21": {"PCM0_DIN", "SIM_VPPPP", "", "", "PA_EINT21"},
+	"PC0":  {"NAND_WE", "SPI0_MOSI"},
+	"PC1":  {"NAND_ALE", "SPI0_MISO"},
+	"PC2":  {"NAND_CLE", "SPI0_CLK"},
+	"PC3":  {"NAND_CE1", "SPI0_CS"},
+	"PC4":  {"NAND_CE0"},
 }
 
 // mapA20Pins uses mappingA20 to actually set the altFunc fields of all gpio
